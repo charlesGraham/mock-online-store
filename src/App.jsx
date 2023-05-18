84a4a1fa05a4bbfa8f1b99ad2d2b6f6e692af00c
Charles Graham <crg0905@gmail.com>
2023-05-18 02:07:19 -0500

    Added some navigation behavior

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 function App() {
+  const Shop = () => {
+    return <h1>I am the Shop page</h1>;
+  }
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='/shop' element={<Shop />} />
+      </Route>
     </Routes>
 
   )
