60c63704ce96ba8b522a73580ccaf1f3ace09621
Charles Graham <crg0905@gmail.com>
2023-05-17 23:57:18 -0500

    Added images to each category

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,30 @@ function App() {
           <h2>Hats</h2>
           <p>Shop</p>
         </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
         <div className="category-body-container">
           <h2>Jackets</h2>
           <p>Shop</p>
         </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
         <div className="category-body-container">
           <h2>Sneakers</h2>
           <p>Shop</p>
         </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
         <div className="category-body-container">
           <h2>Womens</h2>
           <p>Shop</p>
         </div>
+      </div>
+      <div className="category-container">
+        {/* <img /> */}
         <div className="category-body-container">
           <h2>Mens</h2>
           <p>Shop</p>
