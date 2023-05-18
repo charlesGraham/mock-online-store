89d0208c47e482273043224c4c3611c639e091a0
Charles Graham <crg0905@gmail.com>
2023-05-17 23:55:01 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        {/* <img /> */}
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Womens</h2>
+          <p>Shop</p>
+        </div>
+        <div className="category-body-container">
+          <h2>Mens</h2>
+          <p>Shop</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
