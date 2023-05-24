8989898cbde9e34921897fdebf5eabd40c7a9651
Charles Graham <crg0905@gmail.com>
2023-05-24 17:15:00 -0500

    Finished styling sign up/sign in page

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
+import './sign-in-form.styles.scss';
+
+import { signInWithGooglePopup, createUserDocFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase';
+
+const SignInForm = () => {
+  const defaultFormFields = {
+    email: '',
+    password: '',
+  }
+
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //TODO: check if user was authenticated with email and password
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password);
+      console.log(response);
+      resetFormFields();
+    } catch (err) {
+      switch (err.code) {
+        case 'auth/wrong-password':
+          alert('Incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('No user associated with this email');
+          break;
+        default:
+          console.log(err);
+      }
+    }
+  }
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocFromAuth(user);
+  }
+
+
+
+  return (
+    <div className='sign-up-container'>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput label="Email" type="email" onChange={handleChange} name="email" value={email} required />
+        <FormInput label="Password" type="password" onChange={handleChange} name="password" value={password} required />
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle} >Google Sign In</Button>
+        </div>
+
+      </form>
+    </div>
+  );
+}
+
+export default SignInForm;
